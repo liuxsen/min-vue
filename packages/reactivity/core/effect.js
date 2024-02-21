aad316b441710f98f194f3d5342c955b110962c2
jianhui <liujh1@fosun.com>
2024-02-21 19:09:37 +0800

    feat: 建立明确的响应式绑定关系；

--- packages/reactivity/core/effect.js
+++ packages/reactivity/core/effect.js
@@ -1,16 +1,41 @@
-const bucket = new Set()
+const bucket = new WeakMap()
 let activeEffect // 保存被注册的副作用函数
 
 const data = { text: 'hello world' }
 
+/**
+ * 解决问题：
+ * 1. 建立明确的响应式绑定关系；
+ * weakMap
+ *    |__target map[target]
+ *          |_____text1  set
+ *                  |____effectFn
+ *                  |____effectFn2
+ * 数据结构使用WeakMap
+ */
+
 const obj = new Proxy(data, {
-  get(target, k) {
-    bucket.add(activeEffect)
-    return target[k]
+  get(target, key) {
+    if (!activeEffect)
+      return target[key]
+    let depsMap = bucket.get(target)
+    if (!depsMap) {
+      bucket.set(target, (depsMap = new Map()))
+    }
+    let deps = depsMap.get(key)
+    if (!deps) {
+      depsMap.set(key, (deps = new Set()))
+    }
+    deps.add(activeEffect)
+    return target[key]
   },
-  set(target, k, value) {
-    target[k] = value
-    bucket.forEach(fn => fn())
+  set(target, key, value) {
+    target[key] = value
+    const depsMap = bucket.get(target)
+    if (!depsMap)
+      return
+    const effects = depsMap.get(key)
+    effects && effects.forEach(fn => fn())
     return true
   },
 })
@@ -21,7 +46,9 @@ export function effect(fn) {
 }
 
 effect(() => {
+  console.log('effect run')
   document.body.innerHTML = obj.text
 })
 
-obj.text = 'vue3 !!!'
+// obj.text = 'vue3 !!!'
+obj.name = 'name'
