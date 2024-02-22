2395e47d5b695ae2c0d57c54009e73a6d8db624b
jianhui <liujh1@fosun.com>
2024-02-22 18:51:25 +0800

    feat: 将收集副作用函数，执行副作用函数封装

--- packages/reactivity/core/effect.js
+++ packages/reactivity/core/effect.js
@@ -11,35 +11,44 @@ const data = { text: 'hello world' }
  *          |_____text1  set
  *                  |____effectFn
  *                  |____effectFn2
- * 数据结构使用WeakMap
+ * 2. 将收集副作用函数，执行副作用函数封装
+ *    封装 track trigger两个函数; 仅仅是将set， get的逻辑封装到函数中
  */
 
 const obj = new Proxy(data, {
   get(target, key) {
-    if (!activeEffect)
-      return target[key]
-    let depsMap = bucket.get(target)
-    if (!depsMap) {
-      bucket.set(target, (depsMap = new Map()))
-    }
-    let deps = depsMap.get(key)
-    if (!deps) {
-      depsMap.set(key, (deps = new Set()))
-    }
-    deps.add(activeEffect)
+    track(target, key)
     return target[key]
   },
   set(target, key, value) {
-    target[key] = value
-    const depsMap = bucket.get(target)
-    if (!depsMap)
-      return
-    const effects = depsMap.get(key)
-    effects && effects.forEach(fn => fn())
+    trigger(target, key, value)
     return true
   },
 })
 
+// track函数 收集副作用函数
+function track(target, key) {
+  if (!activeEffect)
+    return target[key]
+  let depsMap = bucket.get(target)
+  if (!depsMap) {
+    bucket.set(target, (depsMap = new Map()))
+  }
+  let deps = depsMap.get(key)
+  if (!deps) {
+    depsMap.set(key, (deps = new Set()))
+  }
+  deps.add(activeEffect)
+}
+// trigger
+function trigger(target, key, value) {
+  target[key] = value
+  const depsMap = bucket.get(target)
+  if (!depsMap)
+    return
+  const effects = depsMap.get(key)
+  effects && effects.forEach(fn => fn())
+}
 export function effect(fn) {
   activeEffect = fn
   fn()
