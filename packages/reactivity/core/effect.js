9c6e2fd5b37bfd05fe05cd4a9d23133da40ec049
jianhui <liujh1@fosun.com>
2024-02-26 16:31:46 +0800

    feat: effect嵌套问题

--- packages/reactivity/core/effect.js
+++ packages/reactivity/core/effect.js
@@ -1,8 +1,3 @@
-const bucket = new WeakMap()
-let activeEffect // 保存被注册的副作用函数
-
-const data = { text: 'hello world' }
-
 /**
  * 解决问题：
  * 1. 建立明确的响应式绑定关系；
@@ -13,7 +8,25 @@ const data = { text: 'hello world' }
  *                  |____effectFn2
  * 2. 将收集副作用函数，执行副作用函数封装
  *    封装 track trigger两个函数; 仅仅是将set， get的逻辑封装到函数中
+ * 3. 问题：在三元表达式中，会产生遗留的副作用函数
+ * 解决方案：
+ *  再trigger之前先将key对应的依赖副作用函数清空，然后重新执行
+ * activeEffect.deps = [set[activeEffect]]
+ * key1 set[effect1, effect2]
+ * key2 set[effect1, effect2]
+ * effect1.deps = [set[effect1, effect2], set[effect1, effect2]]
+ * 第一步收集依赖到set中
+ * 第二步收集完成的set集合 push到 activeEffect.deps 中
+ * 第三步 触发proxy的set动作，触发trigger函数；再次执行effectFn；清除在deps中的effectFn；达到副作用函数清除任务
+ * 4. 问题： effect函数嵌套问题
+ *  解决方案： 使用effectStack
  */
+
+const bucket = new WeakMap()
+const effectStack = [] // 副作用函数栈
+let activeEffect // 保存被注册的副作用函数
+
+const data = { text: 'hello world', ok: true }
 
 const obj = new Proxy(data, {
   get(target, key) {
@@ -39,6 +52,7 @@ function track(target, key) {
     depsMap.set(key, (deps = new Set()))
   }
   deps.add(activeEffect)
+  activeEffect.deps.push(deps)
 }
 // trigger
 function trigger(target, key, value) {
@@ -47,17 +61,38 @@ function trigger(target, key, value) {
   if (!depsMap)
     return
   const effects = depsMap.get(key)
-  effects && effects.forEach(fn => fn())
+  const effectsToRun = new Set(effects)
+  effectsToRun.forEach(fn => fn())
 }
+
 export function effect(fn) {
-  activeEffect = fn
-  fn()
+  const effectFn = () => {
+    // 调用cleanup完成清除工作
+    cleanup(effectFn)
+    activeEffect = effectFn
+    effectStack.push(effectFn)
+    fn()
+    effectStack.pop()
+    activeEffect = effectStack[effectStack.length - 1]
+  }
+  // 用来存储所有与该副作用函数相关联的的依赖集合
+  effectFn.deps = []
+  effectFn()
+}
+
+function cleanup(effectFn) {
+  for (let i = 0; i < effectFn.deps.length; i++) {
+    const deps = effectFn.deps[i]
+    deps.delete(effectFn)
+  }
+  effectFn.deps.length = 0
 }
 
 effect(() => {
-  console.log('effect run')
-  document.body.innerHTML = obj.text
+  effect(() => {
+    console.log(obj.text)
+  })
+  console.log(obj.ok)
 })
 
-// obj.text = 'vue3 !!!'
-obj.name = 'name'
+obj.ok = false
